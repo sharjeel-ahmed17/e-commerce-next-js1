0675cb73860565ebe85c0ac238315923fb8a9a3f
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-12 16:11:48 +0500

    hello

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,13 +1,20 @@
 export default function LoginPage() {
     return (
         <div className="flex justify-center items-center h-screen bg-slate-600">
-            <div>
+            <div className="w-[400px]">
                 <div>logo</div>
-                <div className="shadow-lg rounded-md p-3 bg-white">
+                <div className="shadow-lg rounded-md p-6 bg-white">
                     <div>
                         <form>
+                            <p>Login with email</p>
                             <div className="mb-2">
-
+                                <input type="text" placeholder="enter your email" className="p-1 w-full border border-gray-700 rounded-lg" />
+                            </div>
+                            <div className="mb-2">
+                                <input type="text" placeholder="enter your password" className="p-1 w-full border border-gray-700 rounded-lg" />
+                            </div>
+                            <div>
+                                <button>login</button>
                             </div>
                         </form>
                     </div>
